18097b7eb255b032f09a9f458918658c88e00ea7
jobeau <jordboudreau@gmail.com>
2018-01-17 21:39:54 +0000

    add routes for home and campgrounds page

--- app.js
+++ app.js
@@ -3,24 +3,26 @@ var app = express();
 var bodyParser = require("body-parser");
 var request = require('request');
 
+var campgrounds = [
+    { name: "Yosemite", image: "pic" },
+    { name: "Big Sur", image: "pic" },
+    { name: "Hume Lake", image: "pic" },
+    { name: "Yellowstone", image: "pic" }
+];
+
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-    res.send("This is the home page.");
+    res.render('index');
 });
 
-
-
-
-
-
-
-
-
-
+app.get("/campgrounds", function(req, res) {
+    res.render('campgrounds', { campgrounds, campgrounds });
+});
 
 
 
